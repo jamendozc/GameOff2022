98e61f418fa19a89641702f68e563ad8d6c346e8
JOnetto1 <josefina.onetto@sansano.usm.cl>
2022-11-05 17:56:55 -0300

    methods
    added methods for each action button

--- Assets/PanelOpener.cs
+++ Assets/PanelOpener.cs
@@ -14,4 +14,34 @@ public class PanelOpener : MonoBehaviour
             Panel.SetActive(!isActive);
         }
     }
+    public void GatherFood()
+    {
+        Debug.Log("Gather Food");
+        OpenPanel();
+    }
+    public void GatherFlower()
+    {
+        Debug.Log("Gather Flowers");
+        OpenPanel();
+    }
+    public void GatherRock()
+    {
+        Debug.Log("Gather Rocks");
+        OpenPanel();
+    }
+    public void GatherWood()
+    {
+        Debug.Log("Gather Wood");
+        OpenPanel();
+    }
+    public void Cook()
+    {
+        Debug.Log("Cooking");
+        OpenPanel();
+    }
+    public void MakeMedicine()
+    {
+        Debug.Log("Making Medicine");
+        OpenPanel();
+    }
 }
